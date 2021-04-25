bbff0decc3004787d17f6a1a33d0a2b880b780b8
Weijie <52730904+simweijie@users.noreply.github.com>
2021-04-25 17:55:30 +0800

    displays error msg if no result, fixed timezone

--- code/index.py
+++ code/index.py
@@ -3,6 +3,7 @@ import logging
 import pymysql
 import json
 import os
+from pymysql import NULL
 
 #rds settings
 rds_endpoint = os.environ['rds_endpoint']
@@ -26,11 +27,13 @@ logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 def handler(event, context):
     cur = connection.cursor()  
 ## Retrieve Data
-    # note hardcode -16hr due to UTC time
+    # note hardcode +8hr due to USA time
+    print("inputs:")
+    print("clinicId: " + event['clinicId'] + "   district: " + event['district'])
     query = "SELECT b.*,op.opens,op.closes, count(q.id) \
         FROM Branch b, Clinic c, OpeningHours op, Queue q \
         WHERE b.clinicId=c.id AND b.id=op.branchId AND b.id=q.branchId \
-        AND c.id='{}' AND b.district='{}' AND current_time- interval 16 hour BETWEEN opens AND closes \
+        AND c.id='{}' AND b.district='{}' AND current_time+ interval 8 hour BETWEEN opens AND closes \
         AND op.dayOfWeek=dayofweek(now())".format(event['clinicId'],event['district'])    
     cur.execute(query)
     connection.commit()
@@ -38,33 +41,31 @@ def handler(event, context):
     
     branchQueue = []
     rows = cur.fetchall()
+    empty = False
     for row in rows:
         print("TEST {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11]))
-        transactionResponse = {}
-        transactionResponse['branchId'] = row[0]
-        transactionResponse['branchName'] = row[1]
-        transactionResponse['district'] = row[2]
-        transactionResponse['addr'] = row[3]
-        transactionResponse['postal'] = row[4]
-        transactionResponse['contactNo'] = row[5]
-        transactionResponse['latt'] = row[6]
-        transactionResponse['longt'] = row[7]
-        transactionResponse['clinicId'] = row[8]
-        transactionResponse['opens'] = str(row[9])
-        transactionResponse['closes'] = str(row[10])
-        transactionResponse['queueLength'] = row[11]
-        branchQueue.append(transactionResponse)
+        if(row[0] == None):
+            empty = True;
+        else:
+            transactionResponse = {}
+            transactionResponse['branchId'] = row[0]
+            transactionResponse['branchName'] = row[1]
+            transactionResponse['district'] = row[2]
+            transactionResponse['addr'] = row[3]
+            transactionResponse['postal'] = row[4]
+            transactionResponse['contactNo'] = row[5]
+            transactionResponse['latt'] = row[6]
+            transactionResponse['longt'] = row[7]
+            transactionResponse['clinicId'] = row[8]
+            transactionResponse['opens'] = str(row[9])
+            transactionResponse['closes'] = str(row[10])
+            transactionResponse['queueLength'] = row[11]
+            branchQueue.append(transactionResponse)
 
 # Construct http response object
     responseObject = {}
-    # responseObject['statusCode'] = 200
-    # responseObject['headers'] = {}
-    # responseObject['headers']['Content-Type']='application/json'
-    # responseObject['headers']['Access-Control-Allow-Origin']='*'
-    responseObject['data']= branchQueue
-    # responseObject['body'] = json.dumps(transactionResponse, sort_keys=True,default=str)
-    
-    #k = json.loads(responseObject['body'])
-    #print(k['uin'])
-
+    if(empty):
+        responseObject['error']= 'No clinic available'
+    else:
+        responseObject['data']= branchQueue
     return responseObject
